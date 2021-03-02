3e7c41049bd246decd44f2eac94edaa2d51cbe9a
Leo <ooleon@hotmail.com>
2021-03-02 12:33:37 -0300

    Update project

--- src/test/java/localhost/viamongo/ApplicationTests.java
+++ src/test/java/localhost/viamongo/ApplicationTests.java
@@ -18,45 +18,45 @@ import org.springframework.boot.test.context.SpringBootTest;
 //@SpringBootTest
 public class ApplicationTests {
 
-	@Test
-	public void form() {
-		System.out.println("teamshift");
-		String d = "/home/doc/opt/webdriver/geckodriver";
-		System.out.println(d);
+    @Test
+    public void form() {
+        System.out.println("teamshift");
+        String d = "/home/doc/opt/webdriver/geckodriver";
+        System.out.println(d);
         System.setProperty("webdriver.gqecko.driver", d); 
 
         long duration = Duration.ofSeconds(10).getSeconds();
         WebDriver driver = new FirefoxDriver();
         WebDriverWait wait = new WebDriverWait(driver, duration);
         try {
-        	
-        	driver.manage().window().maximize();
-        	driver.manage().deleteAllCookies();
-        	driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
-        	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+            
+            driver.manage().window().maximize();
+            driver.manage().deleteAllCookies();
+            driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+            driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
             driver.get("http://teamshift-qa.crossknowledge.com/");
 
-//      	clik Log In
+//          click Log In
             driver.findElement(By.xpath("//button[contains(text(),'Log In')]")).click();
 //            driver.findElement(By.linkText("Log In")).click();
             
             driver.manage().timeouts().setScriptTimeout(2, TimeUnit.SECONDS);
             
-//          	insert user text into input login-form__login
+//              insert user text into input login-form__login
 //            driver.findElement(By.id("login-form__login")).sendKeys("ostyleon@gmail.com");
             driver.findElement(By.xpath("//input[@id='login-form__login']")).sendKeys("ostyleon@gmail.com");
             
-//      	clik next
+//          click next
 //            driver.findElement(By.linkText("Next")).click();
 //            driver.findElement(By.xpath("//button[@textContent='Next']")).click();
             driver.findElement(By.xpath("//button[contains(text(),'Next')]")).click();
 
-//      	insert password text into input login-form__password
+//          insert password text into input login-form__password
 //            driver.findElement(By.id("login-form__password")).sendKeys("WLS2020qa");
 //            driver.findElement(By.tagName("member_password")).sendKeys("WLS2020qa");
             driver.findElement(By.xpath("//input[@id='login-form__password']")).sendKeys("");
 
-//      	clik LOGIN
+//          click LOGIN
 //          driver.findElement(By.linkText("Login")).click();
 //          driver.findElement(By.xpath("//button[@textContent='Login']")).click();
           driver.findElement(By.xpath("//button[contains(text(),'Login')]")).click();
@@ -69,7 +69,7 @@ public class ApplicationTests {
             driver.quit();
         }
 
-	}
-	
+    }
+    
 
 }
